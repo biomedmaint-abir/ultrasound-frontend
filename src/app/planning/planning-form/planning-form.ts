62b2fcaf21c44380631ed11c4a6765ce617779de
PFE <pfe@MacBook-Air-de-abir.local>
2026-05-24 15:27:20 +0100

    All fields required in planning form

--- src/app/planning/planning-form/planning-form.ts
+++ src/app/planning/planning-form/planning-form.ts
@@ -86,7 +86,7 @@ export class PlanningForm implements OnInit {
     return n === 1 ? '1ere' : n + 'eme';
   }
 
-  get isFormValid(): boolean { return !!(this.planning.client && this.planning.ville && this.planning.appareil && this.planning.annee); }
+  get isFormValid(): boolean { return !!(this.planning.client && this.planning.ville && this.planning.appareil && this.planning.annee && this.planning.marque && this.planning.numeroSerie && this.planning.responsable && this.planning.frequence); }
   isInvalid(field: any): boolean { return this.submitted && !field; }
 
   genererPDF(): void {
